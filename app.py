21b0ff039f5eea12e2d4af4245734b8b5e4b3271
Alicia-09 <24308060610613@cetis61.edu.mx>
2025-10-15 09:16:13 -0600

    nuevo formulario

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/animales")
+def animales():
+    return render_template("animales.html")
+
+@app.route("/vehiculos")
+def carros():
+    return render_template("vehiculos.html")
+
+@app.route("/maravillas")
+def maravillas():
+    return render_template("maravillas.html")
+
+@app.route("/acerca")
+def acerca():
+    return render_template("acerca.html")
+
+@app.route("/formulario")
+def inicio():
+    return render_template("formulario.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
